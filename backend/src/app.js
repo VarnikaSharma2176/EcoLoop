f532de529e18656d77ecfcb149f8299fe7530430
VarnikaSharma2176 <varnikasharma2006@gmail.com>
2026-07-05 19:49:36 +1000

    feat(auth): add JWT authentication middleware
    - Added JWT authentication middleware
    - Protected user profile route
    - Added user profile controller
    - Connected user routes
    - Verified protected API access using Postman

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,13 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
-
 app.use(cookieParser());
 
 app.get("/", (req, res) => {
@@ -22,5 +20,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
 
 export default app;
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,45 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+export const protect = async (req, res, next) => {
+  try {
+    let token;
+
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
+
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Access denied. Please login.",
+        errors: [],
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found.",
+        errors: [],
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token.",
+      errors: [],
+    });
+  }
+};
